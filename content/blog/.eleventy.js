9405b9893d3840322a018b9553c297503514fe70
shivamanagementinc-pixel <shiva.management.inc@gmail.com>
2026-07-22 21:36:02 -0400

    Create .eleventy.js

---
+++ content/blog/.eleventy.js
@@ -0,0 +1,33 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPassthroughCopy("admin");
+  eleventyConfig.addPassthroughCopy("_redirects");
+  eleventyConfig.addPassthroughCopy("netlify.toml");
+
+  eleventyConfig.addFilter("dateDisplay", (date) =>
+    new Intl.DateTimeFormat("en-CA", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      timeZone: "UTC",
+    }).format(date)
+  );
+
+  eleventyConfig.addCollection("blog", (collectionApi) =>
+    collectionApi
+      .getFilteredByGlob("content/blog/*.md")
+      .sort((firstPost, secondPost) => secondPost.date - firstPost.date)
+  );
+
+  return {
+    dir: {
+      input: ".",
+      includes: "_includes",
+      layouts: "_includes/layouts",  // ← THIS WAS MISSING!
+      output: "_site",
+    },
+    templateFormats: ["md", "html", "njk"],
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk"
+  };
+};
